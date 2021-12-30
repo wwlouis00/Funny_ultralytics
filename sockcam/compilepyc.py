0aa1e7f53d2577a3ccebcd79c28c73349c3b04ac
unknown <wwlouis00@gmail.com>
2021-12-30 17:55:13 +0800

    update file

--- sockcam/compilepyc.py
+++ sockcam/compilepyc.py
@@ -8,6 +8,6 @@ for f in files:
     if f == __file__:
         continue
     elif ".py" in f:
-        py_compile.compile(f,f"./socket_cam/{f[0:-3]}.pyc")
+        py_compile.compile(f,f"./socketcam_pyc/{f[0:-3]}.pyc")
         print(f"Success compile the {f} to {f[0:-3]}.pyc")
  

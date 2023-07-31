8e15569cede76c6b98c196f0fa64c0fa00957a87
wwlouis00 <wwlouis00@gmail.com>
2023-08-01 03:55:22 +0800

    Update yolov8_realtime.py

--- yolov8_realtime.py
+++ yolov8_realtime.py
@@ -1,15 +1,9 @@
 from ultralytics import YOLO
-# from ultralytics.yolo.v8.detect.predict import SegmentationPredictor
-# from ultralytics.yolo.v8.detect.predict import DetectionPredictor
-# import cv2
+
+
 model = YOLO("model/yolov8n-seg.pt")
-# # model.predict("",show=True,conf=0.5)
+
+# realtime camera
 model.predict(0,show=True,conf=0.4)
-
-
-#from ultralytics import YOLO
-#import cv2
-
-#model = YOLO("box_seg.pt")
-
-#model.predict("download.jpeg",show=True,save=True)
+# picture
+model.predict("download.jpeg",show=True,save=True)

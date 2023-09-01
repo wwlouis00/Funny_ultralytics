bc63c69b1d7066d4d33655421741a70418165f55
wwlouis00 <wwlouis00@gmail.com>
2023-09-01 14:16:30 +0800

    Delete yolov8 directory

--- yolov8/yolov8_realtime.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-
-model = YOLO("model/yolov8n-seg.pt")
-
-# realtime camera
-model.predict(0,show=True,conf=0.4)
-# picture
-model.predict("download.jpeg",show=True,save=True)

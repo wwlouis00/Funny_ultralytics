e378f5574e9f69ad0e469476fbb3e5503d365310
wwlouis00 <wwlouis00@gmail.com>
2023-11-03 15:06:07 +0800

    Update sam_b_realtime.py

--- sam_b_realtime.py
+++ sam_b_realtime.py
@@ -1,3 +1,8 @@
 from ultralytics import SAM
 model = SAM("sam_b.pt")
-model.predict("images/jean.jpeg",show=True)
+
+# camera
+model.predict("0",show=True)
+
+# picture
+model.predict("your image",show=True)
